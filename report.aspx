f957af70e94d3f690390424506956e500c85c500
SedanurKatran <sedanurkatran@anadolu.edu.tr>
2018-02-14 23:11:33 +0300

    Add files via upload

---
+++ report.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="report.aspx.cs" Inherits="report" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+   
+    
+        <asp:Label ID="Label1" runat="server" Text="Report"></asp:Label>
+        <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="True"  Height="1202px" ReportSourceID="CrystalReportSource1"  ToolPanelView="None" ToolPanelWidth="200px" Width="903px" ReuseParameterValuesOnRefresh="True" />
+        <CR:CrystalReportSource ID="CrystalReportSource1" runat="server">
+            <Report FileName="CrystalReport.rpt">
+            </Report>
+        </CR:CrystalReportSource>
+   
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ report.aspx.cs
@@ -0,0 +1,33 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+
+using System.Configuration;
+using System.Data.SqlClient;
+using CrystalDecisions.Shared;
+
+
+public partial class report : System.Web.UI.Page
+{ 
+
+    protected void Page_Load(object sender, EventArgs e)
+    {   
+        if (!IsPostBack)
+        {
+            ReportDocument reportdocument = new ReportDocument();
+            reportdocument.Load(Server.MapPath("~/CrystalReport.rpt"));
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString)) 
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Ogrenci", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "Ogrenci");
+                con.Close();
+            }
+            reportdocument.SetDataSource(ds);
+            CrystalReportViewer1.ReportSource = reportdocument;
+        }
+    }
+}
+
